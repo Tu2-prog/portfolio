1de8e3bb4136384066d6f59ce16cbc520f185885
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-09-12 10:55:53 +0200

    add skills into the skill section of portfolio

---
+++ src/grouped_skills.js
@@ -0,0 +1,55 @@
+import { faHtml5, faCss3, faJs, faReact, faPython, faGithub, faVuejs, faAngular, faGitSquare} from '@fortawesome/free-brands-svg-icons';
+
+export const webdev = [
+    {
+        id:1,
+        title:"HTML5",
+        icon:faHtml5
+    },
+    {
+        id:2,
+        title:"CSS3",
+        icon:faCss3
+    },
+    {
+        id:3,
+        title:"JavaScript",
+        icon:faJs
+    },
+    {
+        id:4,
+        title:"React",
+        icon:faReact,
+    },
+    {
+        id:5,
+        title:"Angular",
+        icon:faAngular,
+    },
+    {
+        id:6,
+        title:"VueJs",
+        icon:faVuejs
+    },
+]
+
+export const python_dev = [
+    {
+        id:1,
+        title:"Python",
+        icon:faPython
+    },
+]
+
+export const general_dev = [
+    {
+        id:1,
+        title:"Git",
+        icon:faGitSquare
+    },
+    {
+        id:2,
+        title:"GitHub",
+        icon:faGithub
+    }
+]
