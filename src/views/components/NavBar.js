e39dd675b7e8b29488b6bb0f955a789d20b800be
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-09-23 08:45:14 +0200

    made navbar responsive and changed tab name

--- src/views/components/NavBar.js
+++ src/views/components/NavBar.js
@@ -1,39 +1,23 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <a className="navbar-brand" href="/">My Portfolio</a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/projects">Projects</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/cv">CV</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/contact_me">Contact</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/skill_set">Skills</a>
-            </li>
-          </ul>
-        </div>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <div className="container"> {/* Centered container */}
+        <Navbar.Brand href="/">My Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto"> {/* Align links to the right */}
+            <Nav.Link href="/projects">Projects</Nav.Link>
+            <Nav.Link href="/cv">CV</Nav.Link>
+            <Nav.Link href="/contact_me">Contact</Nav.Link>
+            <Nav.Link href="/skill_set">Skills</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </div>
-    </nav>
+    </Navbar>
   );
 };
