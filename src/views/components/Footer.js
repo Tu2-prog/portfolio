ae88cc8ce75e4cf70ea156cfe22e9bb7add6815c
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-09-23 16:00:05 +0200

    Revert "sticky footer succes"
    This reverts commit 050e71475ea03752d5c34b5465c64822117fbb5b.

--- src/views/components/Footer.js
+++ src/views/components/Footer.js
@@ -1,33 +1,37 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import { FaTablet, FaMailBulk } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-dark" style={{ position: 'fixed', bottom: '0', width: '100%', whiteSpace: 'pre-wrap' }}>
-      <div className="container" style={{ padding: '20px 0' }}>
-        <div className="row">
-          <div className="col-md-6">
-            <h5 style={{ color: 'white' }}>About me</h5>
-            <p style={{ color: 'white' }}>
-              I am a 21 year-old student studying Computer Science at the University of Heidelberg.
+    <footer className="bg-dark">
+      <Container>
+        <Row>
+          <Col md={6}>
+            <h5 style={{marginTop: '5%', color:'white'}}>About me</h5>
+            <p style={{color:'white'}}>
+                I am a 21 year-old student studying Computer Science at the University of Heidelberg.
             </p>
-          </div>
-          <div className="col-md-3">
-            <h5 style={{ color: 'white' }}>Links</h5>
+          </Col>
+          <Col md={3}>
+            <h5 style={{marginTop: '10%', color:'white'}}>Links</h5>
             <ul className="list-unstyled">
               <li><a href="/">Main</a></li>
-              <li><a href="timeline">Timeline</a></li>
+              <li><a href="timeline">Timline</a></li>
             </ul>
-          </div>
-          <div className="col-md-3">
-            <h5 style={{ color: 'white' }}>Contact</h5>
+          </Col>
+          <Col md={3}>
+            <h5 style={{marginTop: '10%', color:'white'}}>Contact</h5>
             <ul className="list-unstyled">
-              <li><a href="tel:+491775449252"><FaTablet className='mr-2' /> +49 177 544 9252</a></li>
-              <li><a href="mailto:anhtu.nguyen3705@gmail.com"> <FaMailBulk className='mr-2' /> anhtu.nguyen3705@gmail.com</a></li>
+              <li><a href="tel:+491775449252"><FaTablet className='mr-2'/> +49 177 544 9252</a></li>
+              <li><a href="mailto:anhtu.nguyen3705@gmail.com"> <FaMailBulk className='mr-2'/> anhtu.nguyen3705@gmail.com</a></li>
             </ul>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
+      {/* <div className="text-center py-3">
+        &copy; {new Date().getFullYear()} Beispielunternehmen
+      </div> */}
     </footer>
   );
 };
