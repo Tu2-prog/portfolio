bfd468adff20f56c7cd5fe9e09b2bf387acd2793
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-12-29 22:09:38 +0100

    add more info about spammy filter and add visuals from it

--- src/views/projects/SpammyFilter.js
+++ src/views/projects/SpammyFilter.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { NavBar } from "../components/NavBar";
 import Footer from "../components/Footer";
+import spammy_home from "../../assets/spammy_home.png";
+import spammy_classify from "../../assets/spammy_classify.png";
+import spammy_data from "../../assets/spammy_data.png";
+import "./static/images.css";
 
 export const SpammyFilter = () => {
     return(
@@ -24,7 +28,22 @@ export const SpammyFilter = () => {
                 <li>Count Vectorizer</li>
             </ol>
             Furthermore, I will dockerize these repositories as microservices realised as Docker containers.
+            Installation details are included in the documentation of the perspective microservice.
             </div>
+            <div className="text-box">
+                These are some screenshots from the SpammyFilter project:
+            </div>
+            <img src={spammy_home} alt="Home"/>
+            <div className="text-box">
+                Users can only access this tool only when they provide the correct access code which is set in a seperate microservice for authentification and classification called <a href="https://github.com/Tu2-prog/spam_filtering_analysis">spam_filtering_analysis_backend</a>.
+                From this view one can type in an input prompt that is put into the request body. This text is then classified and saved with the result in the NoSQL database.
+            </div>
+            <img src={spammy_classify} alt="Classify"/>
+            <div className="text-box">
+                Users can only access this view only when they are logged in. To access the data one needs to have a running instance of the microservice called <a href="https://github.com/Tu2-prog/spam_filter_storage">spam_filtering_analysis_storage</a>.
+                From this view one can inspect the data saved in the NoSQL database, delete them or download them in a .csv file.
+            </div>
+            <img src={spammy_data} alt="Data"/>
             <Footer/>
         </>
     )
